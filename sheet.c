cbc43521346af5a9d1025a4a2f46eaf9cca59e7a
Gustavo Chain <gchain@gmail.com>
2013-11-04 23:44:49 +0100

    More sheet wrapping

--- sheet.c
+++ sheet.c
@@ -2,6 +2,9 @@
 #include <libxl.h>
 
 #include "sheet.h"
+#include "format.h"
+
+extern PyTypeObject XLPyFormatType;
 
 static int
 init(XLPySheet *self)
@@ -35,6 +38,49 @@ is_formula(XLPySheet *self, PyObject *args)
 }
 
 static PyObject *
+cell_format(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	FormatHandle fmt = xlSheetCellFormat(self->handler, row, col);
+	if(!fmt) Py_RETURN_NONE;
+
+	XLPyFormat *obj = PyObject_New(XLPyFormat, &XLPyFormatType);
+	obj->handler = fmt;
+	return (PyObject *)obj;
+}
+
+static PyObject *
+set_cell_format(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	XLPyFormat *fmt;
+	if(!PyArg_ParseTuple(args, "iiO!", &row, &col, &XLPyFormatType, &fmt))
+		return NULL;
+
+	xlSheetSetCellFormat(self->handler, row, col, fmt->handler);
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+read_str(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	FormatHandle fmt = NULL;
+	const char *str = xlSheetReadStr(self->handler, row, col, &fmt);
+	if(!str) Py_RETURN_NONE;
+
+	if(!fmt) return Py_BuildValue("(sO)", str, Py_None);
+
+	XLPyFormat *obj = PyObject_New(XLPyFormat, &XLPyFormatType);
+	obj->handler = fmt;
+	return Py_BuildValue("(sO)", str, obj);
+}
+
+static PyObject *
 write_str(XLPySheet *self, PyObject *args)
 {
 	int row, col;
@@ -50,6 +96,23 @@ write_str(XLPySheet *self, PyObject *args)
 }
 
 static PyObject *
+read_num(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	FormatHandle fmt = NULL;
+	double num;
+	num = xlSheetReadNum(self->handler, row, col, &fmt);
+
+	if(!fmt) return Py_BuildValue("(dO)", num, Py_None);
+
+	XLPyFormat *obj = PyObject_New(XLPyFormat, &XLPyFormatType);
+	obj->handler = fmt;
+	return Py_BuildValue("(dO)", num, obj);
+}
+
+static PyObject *
 write_num(XLPySheet *self, PyObject *args)
 {
 	int row, col;
@@ -62,6 +125,267 @@ write_num(XLPySheet *self, PyObject *args)
 		Py_RETURN_FALSE;
 	}
 	Py_RETURN_TRUE;
+}
+
+static PyObject *
+read_bool(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	FormatHandle fmt = NULL;
+	int val = xlSheetReadBool(self->handler, row, col, &fmt);
+
+	if(!fmt) return Py_BuildValue("(OO)",
+			(0 == val) ? Py_False : Py_True, Py_None);
+
+	XLPyFormat *obj = PyObject_New(XLPyFormat, &XLPyFormatType);
+	obj->handler = fmt;
+	return Py_BuildValue("(OO)",
+			(0 == val) ? Py_False : Py_True, obj);
+}
+
+static PyObject *
+write_bool(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	PyObject *val;
+	if(!PyArg_ParseTuple(args, "iiO!", &row, &col, &PyBool_Type, &val)) {
+		return NULL;
+	}
+
+	if (!xlSheetWriteBool(self->handler, row, col,
+				PyObject_IsTrue(val) ? 1 : 0,
+				0)) {
+		Py_RETURN_FALSE;
+	}
+	Py_RETURN_TRUE;
+}
+
+static PyObject *
+read_blank(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	FormatHandle fmt = NULL;
+	int val = xlSheetReadBlank(self->handler, row, col, &fmt);
+
+	if(!fmt) return Py_BuildValue("(OO)",
+			(0 == val) ? Py_False : Py_True, Py_None);
+
+	XLPyFormat *obj = PyObject_New(XLPyFormat, &XLPyFormatType);
+	obj->handler = fmt;
+	return Py_BuildValue("(OO)",
+			(0 == val) ? Py_False : Py_True, obj);
+}
+
+static PyObject *
+write_blank(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	if (!xlSheetWriteBlank(self->handler, row, col, 0)) {
+		Py_RETURN_FALSE;
+	}
+	Py_RETURN_TRUE;
+}
+
+static PyObject *
+read_formula(XLPySheet *self, PyObject *args)
+{
+	const int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	FormatHandle fmt = NULL;
+	const char *val = xlSheetReadFormula(self->handler, row, col, &fmt);
+
+	if(!fmt) return Py_BuildValue("(sO)", val, Py_None);
+
+	XLPyFormat *obj = PyObject_New(XLPyFormat, &XLPyFormatType);
+	obj->handler = fmt;
+	return Py_BuildValue("(sO)", val, obj);
+}
+
+static PyObject *
+write_formula(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	const char *val;
+	if(!PyArg_ParseTuple(args, "iis", &row, &col, &val)) return NULL;
+
+	if (!xlSheetWriteFormula(self->handler, row, col, val, 0)) {
+		Py_RETURN_FALSE;
+	}
+	Py_RETURN_TRUE;
+}
+
+static PyObject *
+read_comment(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	const char *str = xlSheetReadComment(self->handler, row, col);
+	return Py_BuildValue("s", str);
+}
+
+static PyObject *
+write_comment(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	const char *value, *author = NULL;
+	int width = 0, height = 0;
+	if(!PyArg_ParseTuple(args, "iis|sii", &row, &col, &value, &author,
+				&width, &height)) return NULL;
+
+	xlSheetWriteComment(self->handler, row, col, value, author, width, height);
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+is_date(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	if(xlSheetIsDate(self->handler, row, col))
+		Py_RETURN_TRUE;
+	Py_RETURN_FALSE;
+}
+
+static PyObject *
+read_error(XLPySheet *self, PyObject *args)
+{
+	int row, col;
+	if(!PyArg_ParseTuple(args, "ii", &row, &col)) return NULL;
+
+	return Py_BuildValue("i",
+			xlSheetReadError(self->handler, row, col));
+}
+
+static PyObject *
+col_width(XLPySheet *self, PyObject *args)
+{
+	int col;
+	if(!PyArg_ParseTuple(args, "i", &col)) return NULL;
+	return Py_BuildValue("d", xlSheetColWidth(self->handler, col));
+}
+
+static PyObject *
+row_height(XLPySheet *self, PyObject *args)
+{
+	int row;
+	if(!PyArg_ParseTuple(args, "i", &row)) return NULL;
+	return Py_BuildValue("d", xlSheetRowHeight(self->handler, row));
+}
+
+static PyObject *
+set_col(XLPySheet *self, PyObject *args, PyObject *kwargs)
+{
+	int colFirst, colLast;
+	double width;
+	XLPyFormat *fmt = NULL;
+	PyObject *hidden = NULL;
+	static char *kwlist [] = {
+		"colFirst",
+		"colLast",
+		"width",
+		"format",
+		"hidden",
+		NULL
+	};
+	if(!PyArg_ParseTupleAndKeywords(args, kwargs, "iid|O!O!", kwlist,
+				&colFirst, &colLast, &width,
+				&XLPyFormatType, &fmt,
+				&PyBool_Type, &hidden)) {
+		return NULL;
+	}
+
+	if(!hidden) hidden = Py_False;
+	if(xlSheetSetCol(self->handler, colFirst, colLast, width,
+				(NULL != fmt) ? fmt->handler : 0,
+				PyObject_IsTrue(hidden) ? 1 : 0)) {
+		Py_RETURN_TRUE;
+	}
+	Py_RETURN_FALSE;
+}
+
+static PyObject *
+set_row(XLPySheet *self, PyObject *args, PyObject *kwargs)
+{
+	int row;
+	double height;
+	XLPyFormat *fmt = NULL;
+	PyObject *hidden = NULL;
+	static char *kwlist [] = {
+		"row",
+		"height",
+		"format",
+		"hidden",
+		NULL
+	};
+	if(!PyArg_ParseTupleAndKeywords(args, kwargs, "id|O!O!", kwlist,
+				&row, &height,
+				&XLPyFormatType, &fmt,
+				&PyBool_Type, &hidden)) {
+		return NULL;
+	}
+
+	if(!hidden) hidden = Py_False;
+	if(xlSheetSetRow(self->handler, row, height,
+				(NULL != fmt) ? fmt->handler : 0,
+				PyObject_IsTrue(hidden) ? 1 : 0)) {
+		Py_RETURN_TRUE;
+	}
+	Py_RETURN_FALSE;
+}
+
+static PyObject *
+row_hidden(XLPySheet *self, PyObject *args)
+{
+	int row;
+	if(!PyArg_ParseTuple(args, "i", &row)) return NULL;
+	if(xlSheetRowHidden(self->handler, row))
+		Py_RETURN_TRUE;
+	Py_RETURN_FALSE;
+}
+
+static PyObject *
+set_row_hidden(XLPySheet *self, PyObject *args)
+{
+	int row;
+	PyObject *hidden = NULL;
+
+	if(!PyArg_ParseTuple(args, "iO!", &row, &PyBool_Type, &hidden)) return NULL;
+	if(xlSheetSetRowHidden(self->handler, row,
+				PyObject_IsTrue(hidden) ? 1 : 0))
+		Py_RETURN_TRUE;
+	Py_RETURN_FALSE;
+}
+
+static PyObject *
+col_hidden(XLPySheet *self, PyObject *args)
+{
+	int col;
+	if(!PyArg_ParseTuple(args, "i", &col)) return NULL;
+	if(xlSheetColHidden(self->handler, col))
+		Py_RETURN_TRUE;
+	Py_RETURN_FALSE;
+}
+
+static PyObject *
+set_col_hidden(XLPySheet *self, PyObject *args)
+{
+	int col;
+	PyObject *hidden = NULL;
+
+	if(!PyArg_ParseTuple(args, "iO!", &col, &PyBool_Type, &hidden)) return NULL;
+	if(xlSheetSetColHidden(self->handler, col,
+				PyObject_IsTrue(hidden) ? 1 : 0))
+		Py_RETURN_TRUE;
+	Py_RETURN_FALSE;
 }
 
 static PyObject *
@@ -127,12 +451,77 @@ static PyMethodDef methods[] = {
         "Returns cell's type."},
     {"isFormula", (PyCFunction) is_formula, METH_VARARGS,
         "Checks that cell contains a formula."},
+	{"cellFormat", (PyCFunction) cell_format, METH_VARARGS,
+		"Returns cell's format. It can be changed by user."},
+	{"setCellFormat", (PyCFunction) set_cell_format, METH_VARARGS,
+		"Sets cell's format."},
+	{"readStr", (PyCFunction) read_str, METH_VARARGS,
+		"Reads a string and its format from cell. "
+		"Returns a (text, format) tuple, "
+		"Format will be None if specified cell doesn't contain string or error occurs."},
 	{"writeStr", (PyCFunction) write_str, METH_VARARGS,
 		"Writes a string into cell with specified format (if present). Returns False if error occurs."},
+	{"readNum", (PyCFunction) read_num, METH_VARARGS,
+		"Reads a number or date/time and its format from cell. "
+		"Use Book::dateUnpack() for extract date/time parts. "
+		"Returns a tuple with (num, format)."},
     {"writeNum", (PyCFunction) write_num, METH_VARARGS,
         "Writes a string into cell with specified format. "
         "If format is not present then format is ignored. "
         "Returns False if error occurs."},
+	{"readBool", (PyCFunction) read_bool, METH_VARARGS,
+		"Reads a bool value and its format from cell. "
+		"If format is None then error occurs. "
+		"Returns a tuple with (num, format)."},
+	{"writeBool", (PyCFunction) write_bool, METH_VARARGS,
+		"Writes a bool value into cell with specified format. "
+		"If format is None then format is ignored. "
+		"Returns False if error occurs."},
+	{"readBlank", (PyCFunction) read_blank, METH_VARARGS,
+		"Reads format from blank cell. Returns False if specified cell isn't blank or error occurs."},
+	{"writeBlank", (PyCFunction) write_blank, METH_VARARGS,
+		"Writes blank cell with specified format."},
+	{"readFormula", (PyCFunction) read_formula, METH_VARARGS,
+		"Reads a formula and its format from cell. "
+		"Returns None if specified cell doesn't contain formula or error occurs."},
+	{"writeFormula", (PyCFunction) write_formula, METH_VARARGS,
+		"Writes a formula into cell with specified format. "
+		"If format equals None then format is ignored. "
+		"Returns False if error occurs."},
+	{"readComment", (PyCFunction) read_comment, METH_VARARGS,
+		"Reads a comment from specified cell."},
+	{"writeComment", (PyCFunction) write_comment, METH_VARARGS,
+		"Writes a comment to the cell. Parameters:\n"
+		"(row, col) - cell's position;\n"
+		"value - comment string;\n"
+		"author - author string;\n"
+		"width - width of text box in pixels;\n"
+		"height - height of text box in pixels."},
+	{"isDate", (PyCFunction) is_date, METH_VARARGS,
+		"Checks that cell contains a date or time value."},
+	{"readError", (PyCFunction) read_error, METH_VARARGS,
+		"Reads error from cell."},
+	{"colWidth", (PyCFunction) col_width, METH_VARARGS,
+		"Returns column width."},
+	{"rowHeight", (PyCFunction) row_height, METH_VARARGS,
+		"Returns row height."},
+	{"setCol", (PyCFunction) set_col, METH_KEYWORDS,
+		"Sets column width and format for all columns from colFirst to colLast. "
+		"Column width measured as the number of characters of the maximum digit width of the numbers 0, 1, 2, ..., 9 as rendered in the normal style's font. "
+		"If format equals None then format is ignored. "
+		"Columns may be hidden. Returns False if error occurs"},
+	{"setRow", (PyCFunction) set_row, METH_KEYWORDS,
+		"Sets row height and format. Row height measured in point size. "
+		"If format equals None then format is ignored. "
+		"Row may be hidden. Returns False if error occurs"},
+	{"rowHidden", (PyCFunction) row_hidden, METH_VARARGS,
+		"Returns whether row is hidden."},
+	{"setRowHidden", (PyCFunction) set_row_hidden, METH_VARARGS,
+		"Hides row."},
+	{"colHidden", (PyCFunction) col_hidden, METH_VARARGS,
+		"Returns whether col is hidden."},
+	{"setColHidden", (PyCFunction) set_col_hidden, METH_VARARGS,
+		"Hides col."},
 	{"setMerge", (PyCFunction) set_merge, METH_VARARGS,
 		"Sets merged cells for range: rowFirst - rowLast, colFirst - colLast. "
 		"Returns False if error occurs."},
--- tests/test.sheet.py
+++ tests/test.sheet.py
@@ -16,6 +16,31 @@ class TestSheet(unittest.TestCase):
         self.assertFalse(
                 self.sheet.isFormula(0, 0))
 
+    def test_cellFormat(self):
+        self.assertIsNone(self.sheet.cellFormat(0,0))
+
+    def test_setCellFormat(self):
+        fmt = self.book.addFormat()
+        fmt.setNumFormat(NUMFORMAT_ACCOUNT_D2_CUR)
+
+        self.sheet.setCellFormat(3, 3, fmt)
+        fmt = self.sheet.cellFormat(3, 3)
+
+        # num format should be equals for the same cell
+        self.assertEqual(NUMFORMAT_ACCOUNT_D2_CUR,
+                fmt.numFormat())
+
+    def test_readStr(self):
+        (text, fmt) = self.sheet.readStr(0, 0)
+        self.assertEqual(text,
+                "Created by LibXL trial version. Please buy the LibXL full version for removing this message.")
+        self.assertIsNone(fmt)
+
+        self.sheet.writeStr(2, 2, "Hello!")
+        (text, fmt) = self.sheet.readStr(2, 2)
+        self.assertEqual("Hello!", text)
+        self.assertEqual('XLPyFormat', type(fmt).__name__)
+
     def test_writeStr(self):
         # Trial Version return False on 0,0
         self.assertFalse(
@@ -23,6 +48,12 @@ class TestSheet(unittest.TestCase):
 
         self.assertTrue(
                 self.sheet.writeStr(1, 0, "Hello World"))
+
+    def test_readNum(self):
+        self.sheet.writeNum(3, 3, 200)
+        (num, fmt) = self.sheet.readNum(3, 3)
+        self.assertEqual(200.0, num)
+        self.assertEqual('XLPyFormat', type(fmt).__name__)
 
     def test_writeNum(self):
         self.assertTrue(
@@ -34,27 +65,121 @@ class TestSheet(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.sheet.writeNum(1, 1, "twenty")
 
+    def test_readBool(self):
+        self.assertTrue(self.sheet.writeBool(3, 3, False))
+        self.assertFalse(self.sheet.readBool(3, 3)[0])
+
+        self.assertTrue(self.sheet.writeBool(3, 3, True))
+        self.assertTrue(self.sheet.readBool(3, 3)[0])
+
+    def test_writeBool(self):
+        # error with unregistered lib
+        self.assertFalse(self.sheet.writeBool(0, 0, True))
+        self.assertTrue(self.sheet.writeBool(1, 1, True))
+        self.assertTrue(self.sheet.writeBool(1, 1, False))
+
+    def test_readBlank(self):
+        (val, fmt) = self.sheet.readBlank(0, 0)
+        self.assertFalse(val)
+        self.assertIsNone(fmt)
+
+    def test_writeBlank(self):
+        self.assertFalse(self.sheet.writeBlank(0, 0))
+        self.sheet.writeBlank(3, 3)
+        (val, fmt) = self.sheet.readBlank(3, 3)
+        self.assertTrue(val)
+
+    def test_readFormula(self):
+        self.assertEqual(
+                (None, None),
+                self.sheet.readFormula(0, 0)
+        )
+
+    def test_writeFormula(self):
+        # can't write row 0 in trial version
+        self.assertFalse(self.sheet.writeFormula(0, 0, '=3+3'))
+
+        # incorrect token in formula
+        self.assertFalse(self.sheet.writeFormula(1, 0, 'incorrect_token'))
+
+        self.assertTrue(self.sheet.writeFormula(1, 0, '=3+3'))
+        (val, fmt) = self.sheet.readFormula(1, 0)
+        self.assertEqual('3+3', val)
+
+    def test_readComment(self):
+        self.assertIsNone(self.sheet.readComment(0, 0))
+
+    def test_writeComment(self):
+        self.assertIsNone(
+                self.sheet.writeComment(0, 0, 'comment', 'author', 10, 10))
+
+        self.assertEqual('comment',
+                self.sheet.readComment(0, 0))
+
+        self.assertIsNone( self.sheet.writeComment(0, 0,
+            'passing just this param should also work'))
+
+    def test_isDate(self):
+        self.assertFalse(self.sheet.isDate(0, 0))
+
+    def test_readError(self):
+        self.assertEqual(ERRORTYPE_NOERROR, self.sheet.readError(0, 0));
+
+    def test_colWidth(self):
+        val = self.sheet.colWidth(0)
+        self.assertTrue( isinstance(val, float) )
+
+    def test_rowHeight(self):
+        val = self.sheet.rowHeight(0)
+        self.assertTrue( isinstance(val, float) )
+
+    def test_setCol(self):
+        self.assertTrue(self.sheet.setCol(0, 10, 20))
+        self.assertTrue(self.sheet.setCol(0, 10, 20.0, hidden = True))
+
+    def test_setRow(self):
+        self.assertFalse(self.sheet.setRow(0, 20))
+        self.assertTrue(self.sheet.setRow(1, 20))
+        self.sheet.setRow(0, 20.0, hidden = True)
+
+    def test_rowHidden(self):
+        self.assertFalse(self.sheet.rowHidden(0))
+        self.assertFalse(self.sheet.rowHidden(1))
+
+    def test_setRowHidden(self):
+        self.assertFalse(self.sheet.setRowHidden(0, True)) # trial version error
+
+        self.sheet.setRowHidden(1, True)
+        self.assertTrue(self.sheet.rowHidden(1))
+
+        self.sheet.setRowHidden(1, False)
+        self.assertFalse(self.sheet.rowHidden(1))
+
+    def test_colHidden(self):
+        self.assertFalse(self.sheet.colHidden(0))
+        self.assertFalse(self.sheet.colHidden(1))
+
+    def test_setColHidden(self):
+        self.sheet.setColHidden(1, True)
+        self.assertTrue(self.sheet.colHidden(1))
+
+        self.sheet.setColHidden(1, False)
+        self.assertFalse(self.sheet.colHidden(1))
+
     def test_setMerge(self):
-        self.assertTrue(
-                self.sheet.setMerge(0, 0, 3, 3))
-        
-        self.assertFalse(
-                self.sheet.setMerge(0, 0, -3, -3))
+        self.assertTrue(self.sheet.setMerge(0, 0, 3, 3))
+        self.assertFalse(self.sheet.setMerge(0, 0, -3, -3))
 
     def test_setName(self):
-        self.assertIsNone(
-                self.sheet.setName('foo'))
-
+        self.assertIsNone(self.sheet.setName('foo'))
 
     def test_setPicture(self):
         img = self.book.addPicture('./logo.png')
-        self.assertIsNone(
-                self.sheet.setPicture(0, 0, 1, img, 0, 0))
+        self.assertIsNone(self.sheet.setPicture(0, 0, 1, img, 0, 0))
 
     def test_setPicture2(self):
         img = self.book.addPicture('./logo.png')
-        self.assertIsNone(
-                self.sheet.setPicture2(0, 0, 100, 100, img, 0, 0))
+        self.assertIsNone(self.sheet.setPicture2(0, 0, 100, 100, img, 0, 0))
 
 if __name__ == '__main__':
     unittest.main()

75a1b96a6beeee3c61c1a23a081f8a30f916afa6
Gustavo Chain <gchain@gmail.com>
2014-01-09 23:12:06 +0100

    Font wrapper

--- font.c
+++ font.c
@@ -3,6 +3,9 @@
 
 #include "font.h"
 
+typedef void(set_t)(FontHandle, int);
+typedef int (get_t)(FontHandle);
+
 static int
 init(XLPyFont *self)
 {
@@ -15,7 +18,94 @@ dealloc(XLPyFont *self)
 	self->ob_type->tp_free((PyObject*)self);
 }
 
+static PyObject *
+set(XLPyFont *self, PyObject *args, set_t fn)
+{
+	int val;
+	PyArg_ParseTuple(args, "i", &val);
+	fn(self->handler, val);
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+get(XLPyFont *self, get_t fn, int boolean)
+{
+	int val = fn(self->handler);
+	if(!boolean) return Py_BuildValue("i", val);
+	if(val) Py_RETURN_TRUE;
+	Py_RETURN_FALSE;
+}
+
+#define GET(name, fn, b) static PyObject *\
+	name(XLPyFont *self) {\
+		get(self, fn, b);\
+	}
+
+#define SET(name, fn) static PyObject *\
+	set_##name(XLPyFont *self, PyObject *args) {\
+		return set(self, args, fn);\
+	}
+
+#define GETSET(name, get, set, boolean) \
+	GET(name, get, boolean) \
+	SET(name, set)
+
+GETSET(size,       xlFontSize,      xlFontSetSize,      0);
+GETSET(italic,     xlFontItalic,    xlFontSetItalic,    1);
+GETSET(strike_out, xlFontStrikeOut, xlFontSetStrikeOut, 1);
+GETSET(color,      xlFontColor,     xlFontSetColor,     0);
+GETSET(bold,       xlFontBold,      xlFontSetBold,      1);
+GETSET(script,     xlFontScript,    xlFontSetScript,    0);
+GETSET(underline,  xlFontUnderline, xlFontSetUnderline, 0);
+
+static PyObject *
+name(XLPyFont *self)
+{
+	return Py_BuildValue("s", xlFontName(self->handler));
+}
+
+static PyObject *
+set_name(XLPyFont *self, PyObject *args)
+{
+	const char *font;
+	if(!PyArg_ParseTuple(args, "s", &font)) return NULL;
+	xlFontSetName(self->handler, font);
+	Py_RETURN_NONE;
+}
+
 static PyMethodDef methods[] = {
+	{"size", (PyCFunction) size, METH_NOARGS,
+		"Returns the handle of the current font. Returns NULL if error occurs."},
+	{"setSize", (PyCFunction) set_size, METH_VARARGS,
+		"Sets the font for the format. To create a new font use Book::AddFont."},
+	{"italic", (PyCFunction) italic, METH_NOARGS,
+		"Returns whether the font is italic."},
+	{"setItalic", (PyCFunction) set_italic, METH_VARARGS,
+		"Turns on/off the italic font,"},
+	{"strikeOut", (PyCFunction) strike_out, METH_NOARGS,
+		"Returns whether the font is strikeout"},
+	{"setStrikeOut", (PyCFunction) set_strike_out, METH_VARARGS,
+		"Turns on/off the strikeout font."},
+	{"color", (PyCFunction) color, METH_NOARGS,
+		"Returns the color of the font."},
+	{"setColor", (PyCFunction) set_color, METH_VARARGS,
+		"Sets the color of the font."},
+	{"bold", (PyCFunction) bold, METH_NOARGS,
+		"Returns whether the font is bold."},
+	{"setBold", (PyCFunction) set_bold, METH_VARARGS,
+		"Turns on/off the bold font."},
+	{"script", (PyCFunction) script, METH_NOARGS,
+		"Returns the script style of the font."},
+	{"setScript", (PyCFunction) set_script, METH_VARARGS,
+		"Sets the script style of the font."},
+	{"underline", (PyCFunction) underline, METH_NOARGS,
+		"Returns the underline style of the font."},
+	{"setUnderline", (PyCFunction) set_underline, METH_VARARGS,
+		"Sets the underline style of the font."},
+	{"name", (PyCFunction) name, METH_NOARGS,
+		"Returns the name of the font."},
+	{"setName", (PyCFunction) set_name, METH_VARARGS,
+		"Sets the name of the font. Default name is \"Arial\"."},
 	{NULL}
 };
 
---
+++ tests/test.font.py
@@ -0,0 +1,75 @@
+import unittest
+from libxlpy import *
+
+class TestFont(unittest.TestCase):
+    def setUp(self):
+        self.book = Book()
+        self.font = self.book.addFont()
+
+    def testSize(self):
+        self.assertIsInstance(self.font.size(), int)
+
+    def testSetSize(self):
+        self.font.setSize(24)
+        self.assertEquals(self.font.size(), 24)
+
+    def testItalic(self):
+        self.assertIsInstance(self.font.italic(), bool)
+
+    def testSetItalic(self):
+        self.font.setItalic(True)
+        self.assertTrue(self.font.italic())
+        
+        self.font.setItalic(False)
+        self.assertFalse(self.font.italic())
+    
+    def testStrikeOut(self):
+        self.assertIsInstance(self.font.strikeOut(), bool)
+
+    def testSetStrikeOut(self):
+        self.font.setStrikeOut(True)
+        self.assertTrue(self.font.strikeOut())
+        
+        self.font.setStrikeOut(False)
+        self.assertFalse(self.font.strikeOut())
+
+    def testColor(self):
+        self.assertIsInstance(self.font.color(), int)
+
+    def testSetColor(self):
+        self.font.setColor(COLOR_LIGHTORANGE)
+        self.assertEquals(self.font.color(), COLOR_LIGHTORANGE)
+    
+    def testBold(self):
+        self.assertIsInstance(self.font.bold(), bool)
+
+    def testSetBold(self):
+        self.font.setBold(True)
+        self.assertTrue(self.font.bold())
+        
+        self.font.setBold(False)
+        self.assertFalse(self.font.bold())
+
+    def testScript(self):
+        self.assertIsInstance(self.font.script(), int)
+
+    def testSetScript(self):
+        self.font.setScript(SCRIPT_SUPER)
+        self.assertEquals(self.font.script(), SCRIPT_SUPER)
+    
+    def testUnderline(self):
+        self.assertIsInstance(self.font.underline(), int)
+
+    def testSetUnderline(self):
+        self.font.setUnderline(UNDERLINE_DOUBLE)
+        self.assertEquals(self.font.underline(), UNDERLINE_DOUBLE)
+
+    def testName(self):
+        self.assertIsInstance(self.font.name(), str)
+
+    def testSetName(self):
+        self.font.setName("Sans")
+        self.assertEquals("Sans", self.font.name())
+
+if __name__ == '__main__':
+    unittest.main()
